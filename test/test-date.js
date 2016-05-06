02d273e8c4650eab15d1f11e794fb7af99c08ff3
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 11:51:09 -0700

    update copyright notices and license

--- test/test-date.js
+++ test/test-date.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: strong-service-systemd
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var common = require('./common');
 var render = require('../');
 var tap = require('tap');
